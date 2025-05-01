cb21b3696463841b1e6cf07b35c50a5a8f034815
UserOfNames <lightninguy.yoyo@gmail.com>
2025-05-01 10:49:48 -0400

    Degeneracy time (Rust rewrite)

--- flake.nix
+++ flake.nix
@@ -1,16 +1,29 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+  description = "Rust devshell from github:oxalica";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShell.x86_64-linux =
-        pkgs.mkShell { buildInputs = with pkgs; [
-          (python312.withPackages (ps: with ps; [
-            textual
-          ]))
-        ];
-      };
-   };
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+          ];
+
+          shellHook = ''
+            echo "Entered Rust dev environment"
+          '';
+        };
+      }
+    );
 }
