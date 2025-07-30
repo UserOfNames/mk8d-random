0898dada41398b8540b9b8798429188a77db8fd3
UserOfNames <lightninguy.yoyo@gmail.com>
2025-07-29 23:44:27 -0400

    Removed tests (will re-add later) and updated coordinate representation

--- src/courses/course.rs
+++ src/courses/course.rs
@@ -3,14 +3,33 @@ use std::fmt::{self, Display, Formatter};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Coord {
+    row: u8,
+    col: u8,
+    pos: u8,
+}
+
+impl Coord {
+    pub fn new(row: u8, col: u8, pos: u8) -> Self {
+        Coord { row, col, pos }
+    }
+}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}-{}-{}", self.row, self.col, self.pos)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Course {
     pub name: String,
-    pub coord: u32,
+    pub coord: Coord,
     pub rank: u8,
 }
 
 impl Course {
-    pub fn new(rank: u8, coord: u32, name: &str) -> Self {
+    pub fn new(rank: u8, coord: Coord, name: &str) -> Self {
         Course {
             name: name.to_string(),
             coord,
@@ -42,43 +61,5 @@ impl PartialOrd for Course {
 impl Ord for Course {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.rank.cmp(&other.rank)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_course_new() {
-        let course = Course::new(1, 111, "Test1");
-        assert_eq!(course.rank, 1);
-        assert_eq!(course.coord, 111);
-        assert_eq!(course.name, "Test1");
-    }
-
-    #[test]
-    fn test_course_display() {
-        let course = Course::new(2, 112, "Test2");
-        assert_eq!(format!("{}", course), "(112, 02) Test2");
-    }
-
-    #[test]
-    fn test_course_partial_eq() {
-        let course = Course::new(3, 113, "Test3a");
-        let course_eq = Course::new(3, 999, "whatever");
-        let course_not_eq = Course::new(4, 113, "Test3b");
-        assert_eq!(course, course_eq);
-        assert_ne!(course, course_not_eq);
-    }
-
-    #[test]
-    fn test_course_ordering() {
-        let course1 = Course::new(3, 113, "Test4a");
-        let course2 = Course::new(4, 999, "Test4b");
-        assert!(course1 < course2);
-        assert!(course2 > course1);
-        assert!(!(course1 > course2));
-        assert!(!(course2 < course1));
     }
 }
